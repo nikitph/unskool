27391a4d7bedb61ea89e2dd37bd953e88dfc4a23
nikit <nikitph@gmail.com>
2019-02-23 10:09:51 +0530

    create child screen base

--- App/Containers/Styles/CreateChildScreenStyle.js
+++ App/Containers/Styles/CreateChildScreenStyle.js
@@ -2,5 +2,11 @@ import { StyleSheet } from 'react-native'
 import { ApplicationStyles } from '../../Themes/'
 
 export default StyleSheet.create({
-  ...ApplicationStyles.screen
+  ...ApplicationStyles.screen,
+  subTitle: {
+    fontSize: 18,
+    marginTop: 5,
+    fontWeight: '500',
+    color: 'rgba(0,0,0,.5)'
+  }
 })
