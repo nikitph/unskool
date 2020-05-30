ff8d980facd6d8d612d263e76cccf59051945626
Nakul Kundaliya <nakulkundaliya12@gmail.com>
2020-05-30 11:25:02 +0530

    Change initialRoute

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -54,7 +54,7 @@ const PrimaryNav = createStackNavigator({
 }, {
   // Default config for all screens
   headerMode: 'screen',
-  initialRouteName: 'SplashScreen',
+  initialRouteName: 'LaunchScreen',
   navigationOptions: {
     headerStyle: styles.header
   }
