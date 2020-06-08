fa7df5648bfeceaf0886ddcc78e64d6472fea423
Nakul Kundaliya <nakulkundaliya12@gmail.com>
2020-06-08 12:00:17 +0530

    Update converstaions Screen

--- App/Containers/Styles/ConversationsScreenStyle.js
+++ App/Containers/Styles/ConversationsScreenStyle.js
@@ -1,6 +1,43 @@
 import { StyleSheet } from 'react-native'
-import { ApplicationStyles } from '../../Themes/'
+import { ApplicationStyles, Metrics, Colors } from '../../Themes'
 
 export default StyleSheet.create({
-  ...ApplicationStyles.screen
-})
+  ...ApplicationStyles.screen,
+  container: {
+    flex: 1,
+    marginTop: 0,
+    backgroundColor: 'white'
+  },
+  row: {
+    flex: 1,
+    backgroundColor: 'rgba(116,100,78,0.4)',
+    margin: Metrics.smallMargin,
+    marginHorizontal: 20,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+  },
+  boldLabel: {
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    color: Colors.snow,
+    textAlign: 'center',
+    marginBottom: Metrics.smallMargin,
+  },
+  conContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  label: {
+    textAlign: 'center',
+    color: 'rgb(79, 18, 34)',
+    fontSize: 16,
+    fontFamily: 'Avenir',
+  },
+  listContent: {
+    marginTop: 5,
+  },
+});
