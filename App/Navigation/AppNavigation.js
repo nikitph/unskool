602b3d7d4fd0f5b28ffa0c09470c46a76e22a099
Nakul Kundaliya <nakulkundaliya12@gmail.com>
2020-07-23 13:35:14 +0530

    Update Sidemenu

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { createAppContainer } from 'react-navigation'
+import { View, TouchableOpacity, Text} from 'react-native'
+import { createAppContainer, createDrawerNavigator, createSwitchNavigator } from 'react-navigation'
 import CalendarScreen from '../Containers/CalendarScreen'
 import ReviewsScreen from '../Containers/ReviewsScreen'
 import FriendReviewScreen from '../Containers/FriendReviewScreen'
@@ -27,7 +28,7 @@ import LaunchScreen from '../Containers/LaunchScreen'
 import ScreenOne from '../Containers/SignUpFlow/ScreenOne';
 import ScreenTwo from '../Containers/SignUpFlow/ScreenTwo';
 import ScreenThree from '../Containers/SignUpFlow/ScreenThree';
-
+import CustomSideBarScreen from '../Containers/CustomSideBarScreen'
 import SplashScreen from '../Containers/SplashScreen';
 import styles from './Styles/NavigationStyles'
 
@@ -69,4 +70,13 @@ const PrimaryNav = createStackNavigator({
   }
 })
 
-export default createAppContainer(PrimaryNav)
+const DrawerNavigator = createDrawerNavigator(
+  {
+    PrimaryNav: PrimaryNav,
+  },
+  {
+    contentComponent: CustomSideBarScreen,
+  }
+);
+
+export default createAppContainer(DrawerNavigator);
