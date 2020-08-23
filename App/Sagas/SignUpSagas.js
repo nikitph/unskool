50b5176b15d4234d42d8e2405ecd8f418f3f77d4
nikit <nikitph@gmail.com>
2020-08-23 14:43:46 +0530

    local auth working

--- App/Sagas/SignUpSagas.js
+++ App/Sagas/SignUpSagas.js
@@ -11,33 +11,13 @@ export function * signUp ({email, password, alertfunc, nav}) {
     console.log('this is the res', service)
     const res = yield call(client.authenticate, {strategy: 'local', email: email, password: password})
     console.log('this is the res', res)
-
-    // const res = yield call(service.create, {email,password})
-    //console.log("this is the res", res)
-    /* client.service('users').create({email, password})
-       .then((result)=>{
-
-         client.authenticate({
-           strategy: 'local',
-           email: email,
-           password: password
-         }).then(() => {
-           console.log("cool i m logged in ")
-           nav.dispatch(nav.reset([NavigationActions.navigate({routeName: 'ScreenOne'}, {uid :'123'})], 0))
-         }).catch(e => {
-           // Show login page (potentially with `e.message`)
-           console.error('Authentication error', e);
-         });
-
-       })*/
-    /* const response = yield call(client.service('users').create, {email, password})
-     const {uid, displayName, photoURL} = response.user
-     if (uid) {
-       yield put(SignUpActions.signUpSuccess({uid, displayName, photoURL}))
-       const resetAction = nav.reset([NavigationActions.navigate({routeName: 'ScreenOne'}, {uid})], 0)
-       yield put(ChatActions.chatRequest({uid: uid}))
+    if (res.accessToken) {
+      const {_id, email} = res.user
+      yield put(SignUpActions.signUpSuccess({uid: _id, email, accessToken: res.accessToken}))
+      const resetAction = nav.reset([NavigationActions.navigate({routeName: 'ScreenOne'}, {uid: _id})], 0)
+      yield put(ChatActions.chatRequest({uid: _id}))
        yield call(nav.dispatch, resetAction)
-     } else { yield put(SignUpActions.signUpFailure('unknown Error')) }*/
+    } else { yield put(SignUpActions.signUpFailure('unknown Error')) }
   } catch (error) {
     yield put(SignUpActions.signUpFailure(error))
     alertfunc('error', 'Error', error.message)
